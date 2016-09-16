1a0ece31fe4d739bea169009411480805bd28f94
Atsushi-Tago <atsushi_tago@dwango.co.jp>
2016-09-16 15:40:47 +0900

    slim/php-viewを利用してみる

--- public/index.php
+++ public/index.php
@@ -1,8 +1,23 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../src/autoload.php';
 
 $settings = require __DIR__ . '/../conf/slim.php';
 $app = new \Slim\App($settings);
+
+$container = $app->getContainer();
+
+$container['renderer'] = function ($c) {
+	$settings = $c->get('settings')['renderer'];
+	return new Slim\Views\PhpRenderer($settings['template_path']);
+};
+
+$app->get('/{id}', function(\Slim\Http\Request $request, $response) {
+	$id = $request->getAttribute('id');
+	/** @var \Slim\Views\PhpRenderer $renderer */
+	$renderer = $this->renderer;
+	return $renderer->render($response, 'test.html', ['id' => $id]);
+});
 
 $app->run();
