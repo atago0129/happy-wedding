0f24bee4dd45a41a3582c005e140168b02fa9892
atago0129 <atsushi.tago@gmail.com>
2016-09-22 19:10:51 +0900

    statusを変更できるようにした 引き出物選択ページを実装 その他微修正

--- jsx/rsvp.jsx
+++ jsx/rsvp.jsx
@@ -44,11 +44,44 @@ class RsvpModal extends React.Component {
     }
 }
 
+class AlertModal extends React.Component {
+    _handleClick(e) {
+        this.props.closeAlertModal();
+    }
+
+    onRequestClose() {
+        this.props.closeAlertModal();
+    }
+
+    render() {
+        return (
+            <Modal isOpen={this.props.display}
+                   onRequestClose={this.onRequestClose.bind(this)}
+                   style={modalWindowStyle}
+            >
+                <div>
+                    <p>不明なエラーが発生しました。ページをリロードし、しばらくおいてから試してください。</p>
+                    <p>何度もこのメッセージが表示される場合、お手数ですが新郎新婦までご連絡ください。</p>
+                    <p>エラーコード: {this.props.errorCode}</p>
+                    <button className="rsvpButton" onClick={this._handleClick.bind(this)}>確認</button>
+                </div>
+            </Modal>
+        );
+    }
+}
+
+class ConfirmModal extends React.Component {
+    // TODO 出欠確定後のモーダルを作成し、出席なら引き出物ページへ、欠席ならTOPへ飛ばす
+}
+
 class Rsvp extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            rsvpModalDisplay: false
+            rsvpModalDisplay: false,
+            alertModalDisplay: false,
+            errorCode: 'unknown error',
+            userStatus: parseInt(props.status)
         };
     }
 
@@ -64,32 +97,68 @@ class Rsvp extends React.Component {
         });
     }
 
+    openAlertModal() {
+        this.setState({
+            alertModalDisplay: true
+        });
+    }
+
+    setAlertModalErrorCode(errorCode) {
+        this.setState({
+            errorCode: errorCode
+        });
+    }
+
+    closeAlertModal() {
+        this.setState({
+            alertModalDisplay: false
+        });
+    }
+
     requestRsvp(status) {
+        var _this = this;
         Request.post(window.location.href + '/rsvp')
             .send({token: this.props.token, status: status})
             .end((err, res) => {
                 if(err) {
                     console.log(err);
+                    _this.openAlertModal();
                     window.alert('不明なエラーが発生しました。ページをリロードし再度試してください。')
                 } else {
+                    var result = JSON.parse(res.text);
+                    if (result.status !== 'ok') {
+
+                    } else {
+                        _this.setAlertModalErrorCode(result.error.code);
+                        _this.openAlertModal();
+                    }
+
                     console.log(res);
                 }
             });
     }
 
     render() {
+        var presentUrl = location.href + '/present';
         return (
             <section>
                 <div className="rsvp">
-                    <button className="rsvpButton" onClick={this.openModal.bind(this)}>出欠のご回答</button>
+                    {this.state.userStatus === 0 ? <button className="rsvpButton" onClick={this.openModal.bind(this)}>出欠のご回答</button> : null}
+                    {this.state.userStatus === 1 ? <button className="rsvpButton" onClick={this.openModal.bind(this)}>出欠を変更する（御欠席で回答済み）</button> : null}
+                    {(this.state.userStatus === 2 || this.state.userStatus === 10) ? <button className="rsvpButton" onClick={this.openModal.bind(this)}>出欠を変更する（御出席で回答済み）</button> : null}
                 </div>
+                {this.state.userStatus === 2 ?  <div className="rsvp"><a href={presentUrl}><button className="rsvpButton">お土産を選択する</button></a></div> : null}
+                {this.state.userStatus > 0 ?  <div className="rsvp"><p>※出欠は○月○日まで変更可能です</p></div> : null}
                 <RsvpModal display={this.state.rsvpModalDisplay} closeModal={this.closeModal.bind(this)} requestRsvp={this.requestRsvp.bind(this)} />
+                <AlertModal display={this.state.alertModalDisplay} closeModal={this.closeAlertModal.bind(this)} errorCode={this.state.errorCode} />
             </section>
         );
     }
 }
 
+
+
 ReactDom.render(
-    <Rsvp token={document.getElementById('token').getAttribute('data-token')} />,
+    <Rsvp token={document.getElementById('token').getAttribute('data-token')} status={document.getElementById('userStatus').getAttribute('data-user-status')} />,
     document.getElementById('container')
 );
--- public/index.php
+++ public/index.php
@@ -22,10 +22,10 @@ $container['notFoundHandler'] = function ($c) {
 $app->get('/list_test', function(\Slim\Http\Request $request, $response) {
     /** @var \Slim\Views\PhpRenderer $renderer */
     $renderer = $this->renderer;
-    return $renderer->render($response, 'index.html', []);
+    return $renderer->render($response, 'present.html', []);
 });
 
-$app->get('/{id}', function(\Slim\Http\Request $request, $response) {
+$app->get('/{id}', function(\Slim\Http\Request $request, \Slim\Http\Response $response) {
 	$id = $request->getAttribute('id');
 	$user = (new \acolish\model\User())->getUserById($id);
 
@@ -37,37 +37,38 @@ $app->get('/{id}', function(\Slim\Http\Request $request, $response) {
 	$renderer = $this->renderer;
 	return $renderer->render($response, 'invitation.html', [
 	    'name' => $user->getDisplayName(),
+        'status' => $user->getStatus(),
         'token' => (new \acolish\model\Token(\acolish\config\CommonConfig::getInstance()->get('csrf_token_salt')))->generateTokenString($id, time())
     ]);
 });
 
-$app->post('/{id}/rsvp', function ($request, $response) {
-    /** @var \Slim\Http\Request $request */
-    /** @var \Slim\Http\Response $response */
+$app->post('/{id}/rsvp', function (\Slim\Http\Request $request, \Slim\Http\Response $response) {
     $id = $request->getAttribute('id');
 
     $tokenString = $request->getParam('token');
 
     $token = (new \acolish\model\Token(\acolish\config\CommonConfig::getInstance()->get('csrf_token_salt')));
     if (!$token->isValid($id, $tokenString)) {
-        return $response->withJson(['status' => 'ng', 'message' => 'invalid_token'], 401);
+        return $response->withJson(['status' => 'ng', 'error' => ['code' => 'invalid_token']], 401);
     }
 
     $user = (new \acolish\model\User())->getUserById($id);
 
     if ($user === null) {
-        return $response->withJson(['status' => 'ng', 'message' => 'invalid_id'], 401);
+        return $response->withJson(['status' => 'ng', 'error' => ['code' => 'invalid_id']], 401);
     }
 
     $status = intval($request->getParam('status'));
 
     if (!in_array($status, [
-        \acolish\entity\User::STATUS_UNANSWERED,
         \acolish\entity\User::STATUS_NON_PARTICIPANT,
         \acolish\entity\User::STATUS_UNSELECTED_PRESENT,
-        \acolish\entity\User::STATUS_SELECTED_PRESENT,
     ], true)) {
-        return $response->withJson(['status' => 'ng', 'message' => 'invalid_status'], 401);
+        return $response->withJson(['status' => 'ng', 'error' => ['code' => 'invalid_status']], 401);
+    }
+
+    if ($user->getStatus() === $status) {
+        return $response->withJson(['status' => 'ng', 'error' => ['code' => 'invalid_status']], 401);
     }
 
     $user->setStatus($status);
@@ -78,6 +79,26 @@ $app->post('/{id}/rsvp', function ($request, $response) {
 
     return $response->withJson(['status' => 'ok', 'result' => ['id' => $user->getId(), 'status' => $user->getStatus()]], 200);
 
+});
+
+$app->get('/{id}/present', function (\Slim\Http\Request $request, \Slim\Http\Response $response) {
+    $id = $request->getAttribute('id');
+    $user = (new \acolish\model\User())->getUserById($id);
+
+    if ($user === null) {
+        throw new \Slim\Exception\NotFoundException($request, $response);
+    }
+
+    if ($user->getStatus() === \acolish\entity\User::STATUS_UNANSWERED || $user->getStatus() === \acolish\entity\User::STATUS_SELECTED_PRESENT) {
+        $response->withStatus(301)->withHeader('Location', '/' . $id);
+    }
+
+    /** @var \Slim\Views\PhpRenderer $renderer */
+    $renderer = $this->renderer;
+    return $renderer->render($response, 'present.html', [
+        'name' => $user->getDisplayName(),
+        'token' => (new \acolish\model\Token(\acolish\config\CommonConfig::getInstance()->get('csrf_token_salt')))->generateTokenString($id, time())
+    ]);
 });
 
 $app->run();
