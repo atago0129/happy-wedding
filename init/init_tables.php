740cf91b56b259dc4e147681f83dde19816aeb28
atago0129 <atsushi.tago@gmail.com>
2017-04-01 22:06:05 +0900

    bug fix

--- init/init_tables.php
+++ init/init_tables.php
@@ -4,6 +4,8 @@ $setting = require_once __DIR__ . '/../conf/db.php';
 
 $baseSql = 'mysql -u ' . $setting['user'] . ' -h' . $setting['host'] . ' -p' . $setting['password'] . ' ' . $setting['name'];
 
-system($baseSql . '< ../sql/user.sql');
-system($baseSql . '< ../sql/user_gift.sql');
-system($baseSql . '< ../sql/gift.sql');
+$sqlDir = __DIR__ . '/../sql/';
+
+system($baseSql . '< ' . $sqlDir . 'user.sql');
+system($baseSql . '< ' . $sqlDir . 'user_gift.sql');
+system($baseSql . '< ' . $sqlDir . 'gift.sql');
